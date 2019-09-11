1878d5b1218e9ecc0a8c44dcaa1f0651bf04a298
Michael Rapp <michael.rapp@rgi-corp.com>
2019-09-11 14:23:06 +0000

    Quick hack to enable source maps to be used with emp3

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+    mode: "development",
+    entry: "./src/WorldWind.js",
+    devtool: "source-map",
+    output: {
+        path: path.join(__dirname, '../src/mapengine/worldwind'),
+        filename: 'worldwind.min.js',
+    },
+    plugins: [
+        new webpack.IgnorePlugin(/vertx/),
+    ],
+    module: {
+        rules: [
+            { // Rule to babel transpile js to IE compatible js
+                test: /\.js$/,
+                include: [path.join(__dirname, "src")],
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            plugins: ["@babel/plugin-proposal-export-default-from"],
+                        } //babel-loader options
+                    }
+                ]
+            },
+        ]
+    }
+}
